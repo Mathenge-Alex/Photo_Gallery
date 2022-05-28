ca65d21aee5803c8c918b9e027f9201957ef7153
Mathenge-Alex <arxmaina@gmail.com>
2022-05-28 22:53:42 +0300

    Feat: add a class-based view for the home page

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,10 @@
+import django
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from datetime import datetime
 
 # Create your views here.
+
+
+class HomeView(TemplateView):
+    template_name = 'photos/home.html'
