b809dd5ce8e2be17c3a353e89650a0fd4507362a
Mathenge-Alex <arxmaina@gmail.com>
2022-05-31 00:30:55 +0300

    Feat: Add Image search feature

--- photos/views.py
+++ photos/views.py
@@ -4,16 +4,16 @@ from django.views.generic import TemplateView
 from datetime import datetime
 from django.db.models import Q
 from .forms import UploadForm
-
 from .models import Image
-
+from django.db.models.base import ObjectDoesNotExist
+from django.http  import Http404
 # Create your views here.
 
 
-class HomeView(TemplateView):
-    template_name = 'photos/home.html'
+def home(request):
     photos = Image.objects.all()
-    inf = {'photos':photos}
+    image_basket = {'photos':photos}
+    return render(request,'photos/home.html',image_basket)
     
 def upload_picture(request):
     
@@ -27,7 +27,21 @@ def upload_picture(request):
             return redirect('/')
     else:
         form = UploadForm()
-    inf = {
+    image_basket = {
         'form':form
     }
-    return render(request,'pictures/upload_picture.html',inf)
+    return render(request,'photos/upload_picture.html',image_basket)
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_photos = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'photos/search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "Your search was not found. Enter a valid word."
+        return render(request, 'photos/search.html',{"message":message})
+
+
