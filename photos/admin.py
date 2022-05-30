0027d4c04281d2c3daa39f4e28c5e8c3f10c8708
Mathenge-Alex <arxmaina@gmail.com>
2022-05-30 16:30:53 +0300

    Feat: Register admin models

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Image,Category,Location
+
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
