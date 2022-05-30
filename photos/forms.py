2218546bce9abcd88790b9d65cbaaf8566c640a6
Mathenge-Alex <arxmaina@gmail.com>
2022-05-30 16:28:08 +0300

    Feat: add uploading form

--- photos/forms.py
+++ photos/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Image
 
 class UploadForm(forms.ModelForm):
+    """
+    Form for the image model
+    """
     image = forms.ImageField()
     class Meta:
         model = Image
