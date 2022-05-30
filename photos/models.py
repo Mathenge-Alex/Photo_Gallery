a2111b605cddd3d33affb7f051f89d5e120c4cfb
Mathenge-Alex <arxmaina@gmail.com>
2022-05-30 07:01:29 +0300

    Feat: Add image model and methods

--- photos/models.py
+++ photos/models.py
@@ -41,3 +41,47 @@ class Location(models.Model):
     def update_location(cls, id, value):
         cls.objects.filter(id=id).update(name=value)
 
+class Image(models.Model):
+    image = CloudinaryField('image')
+    name = models.CharField(max_length=60)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(null=True)
+    location = models.ForeignKey(Location ,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category ,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+        
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls, id, image):
+        update = cls.objects.filter(id=id).update(image=image)
+        return update
+    
+    @classmethod
+    def update_image_name(cls,id,name):
+        update =cls.objects.filter(id=id).update(name=name)
+        return update
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        image = cls.objects.filter(id=id).all()
+        return image
+
+    class Meta:
+        ordering = ['-upload_date']
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        images = cls.objects.filter(category__name__icontains=search_term)
+        return images
+    
+    @classmethod
+    def filter_by_location(cls, location):
+        image_location = Image.objects.filter(location__name=location).all()
+        return image_location
