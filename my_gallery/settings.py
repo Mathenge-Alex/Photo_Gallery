fb6dbfcb3b98863cb4eba56559e02ddbd8511bc1
Mathenge-Alex <arxmaina@gmail.com>
2022-05-30 16:30:27 +0300

    Conf: Configure bootstrap and images directory

--- my_gallery/settings.py
+++ my_gallery/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     # Added Apps
     'photos',
     'cloudinary',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,14 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR/'static',
 ]
+
+
+import os
+
+#Extra Collectstatic
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
