b2cb4effa8f2e28d540d5616beffd58ec4456000
Mathenge-Alex <arxmaina@gmail.com>
2022-05-30 06:59:23 +0300

    Feat: Create category model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import datetime as dt
+from cloudinary.models import CloudinaryField
+# import cloudinary
+# import cloudinary.uploader
+# import cloudinary.api
 
-# Create your models here.
+# Category Model for different categories of pictures
+class Category(models.Model):
+    
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def save_category(self):
+        self.save()
+        
+    def delete_category(self):
+        self.delete()
+    
+    @classmethod
+    def update_category(cls, cat_id, value):
+        cls.objects.filter(id=cat_id).update(name=value)
+
