4e7a136a75389d3f2686f146a7d9ac907fce2490
Mathenge-Alex <arxmaina@gmail.com>
2022-05-31 00:29:39 +0300

    Config: Add urls for project routes

--- photos/urls.py
+++ photos/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path('', views.home, name='home'),
+    path('uploads',views.upload_picture,name="upload_picture"),
+    path('select', views.query_image, name='query_image'),
+    re_path(r'^search/', views.search_results,name='search_results'),
+    re_path(r'^filter/(\d+)',views.filter_location,name='filter_location'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
