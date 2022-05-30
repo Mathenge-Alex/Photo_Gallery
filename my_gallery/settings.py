d2537a499d62993b44a72a256f4febd4b780673c
Mathenge-Alex <arxmaina@gmail.com>
2022-05-30 06:45:18 +0300

    Config: Configure Claudinary

--- my_gallery/settings.py
+++ my_gallery/settings.py
@@ -14,6 +14,9 @@ from distutils.command.config import config
 from importlib import import_module
 from pathlib import Path
 from decouple import config
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +46,7 @@ INSTALLED_APPS = [
 
     # Added Apps
     'photos',
+    'claudinary',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +136,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config( 
+    cloud_name = "do6nx2k8d", 
+    api_key = "295785855114418", 
+    api_secret = "GegSfX32WzDvwFCOIuH6axAO3aY",
+    secure = True
+    )
