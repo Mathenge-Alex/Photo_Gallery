708eb99c406f75e45f884427f18a16456623f634
Mathenge-Alex <arxmaina@gmail.com>
2022-05-31 17:15:57 +0300

    Config: Edit settings file for heroku deployment.

--- my_gallery/asgi.py
+++ my_gallery/asgi.py
@@ -14,3 +14,6 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_gallery.settings')
 
 application = get_asgi_application()
+
+import django_heroku
+django_heroku.settings(locals())
