ce7edb1a8dafded6d5910e182780f6b26bc13b9d
Mathenge-Alex <arxmaina@gmail.com>
2024-08-16 19:01:15 +0300

    Update dark mode, Home page and image link copying - photos/urls.py

--- photos/urls.py
+++ photos/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('upload/', views.upload_image, name='upload_image'),
     path('search/', views.search_results, name='search_results'),
+    path('delete/<int:image_id>/', views.delete_image, name='delete_image'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
