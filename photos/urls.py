526894f7c83b92cf5e0ac5a6ab7e214f84081684
Mathenge-Alex <arxmaina@gmail.com>
2024-08-16 17:51:46 +0300

    Update Vendors Arena - photos/urls.py

--- photos/urls.py
+++ photos/urls.py
@@ -7,10 +7,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('uploads',views.upload_picture,name="upload_picture"),
-    path('select', views.query_image, name='query_image'),
-    re_path(r'^search/', views.search_results,name='search_results'),
-    re_path(r'^filter/(\d+)',views.filter_location,name='filter_location'),
+    path('upload/', views.upload_image, name='upload_image'),
+    path('search/', views.search_results, name='search_results'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
